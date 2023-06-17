c122ebca827492809dff0053d6a31a6b4171d336
giabach1106 <giabachand@gmail.com>
2023-06-17 20:30:11 +0700

    add

--- server.js
+++ server.js
@@ -22,35 +22,6 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/login", (req, res) => {
-  res.sendFile(__dirname + "/public/login.html");
-});
-
-
-app.post('/login', (req, res) => {
-  var { username, password } = req.body;
-  if (username === 'leviathan' && password === '123456') {
-    res.redirect('/');
-  }
-})
-
-app.get("/sum/:a/:b", (req, res) => {
-  var a = parseInt(req.params.a);
-  var b = parseInt(req.params.b);
-  var sum = a + b;
-  res.send({
-    "tong hai so la": sum
-  })
-})
-
-app.get("/user/:userid", (req, res) => {
-  res.send({
-    userid: req.params.userid,
-    username: "leviathan",
-    phone: " 238956230560",
-    address: "vu pham ham"
-  });
-})
 
 //listen for a connection
 io.on("connection", function (socket) {
@@ -59,23 +30,6 @@ io.on("connection", function (socket) {
   } else {
     console.log("ESP client connected:" + socket.id);
   }
-
-  //topic name: 'phone1', msg is in this format: '0369677432', parse the msg an take last 9 digits
-  socket.on("phone1", function (msg) {
-    console.log("phone1: " + msg);
-    var phone = msg.slice(-9);
-    console.log("phone1: " + phone);
-    socket.broadcast.emit("phone1", phone);
-  });
-
-  //topic name: 'phone2', msg is in this format: '0369677432', parse the msg an take last 9 digits
-  socket.on("phone2", function (msg) {
-    console.log("phone2: " + msg);
-    var phone = msg.slice(-9);
-    console.log("phone2: " + phone);
-    socket.broadcast.emit("phone2", phone);
-  });
-
   //this is message from esp8266 client
   socket.on("message_suckhoe", function (msg) {
     console.log("received message_suckhoe");
