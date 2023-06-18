cb05415845b39a3bca10fd15f23965aed9e49a95
giabach1106 <giabachand@gmail.com>
2023-06-18 14:59:59 +0700

    .

--- public/js/script.js
+++ public/js/script.js
@@ -64,19 +64,3 @@ const imgSliderPrev = document.querySelector('.img-slider-prev');
 const imgSliderDots = document.querySelector('.img-slider-dots');
 const imgSliderDotsItems = document.querySelectorAll('.img-slider-dot');
 
-// const tabs = document.querySelectorAll('[data-tab-target]')
-// const tabContents = document.querySelectorAll('[data-tab-content]')
-
-// tabs.forEach(tab => {
-//   tab.addEventListener('click', () => {
-//     const target = document.querySelector(tab.dataset.tabTarget)
-//     tabContents.forEach(tabContent => {
-//       tabContent.classList.remove('active')
-//     })
-//     tabs.forEach(tab => {
-//       tab.classList.remove('active')
-//     })
-//     tab.classList.add('active')
-//     target.classList.add('active')
-//   })
-// })
