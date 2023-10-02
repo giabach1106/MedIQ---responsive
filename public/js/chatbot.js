c52dc15f312a334d2c1697ab1d3c66b846befef6
giabach1106 <giabachand@gmail.com>
2023-10-02 12:03:33 +0700

    chatbot fixed v2"

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -6,7 +6,7 @@ const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
 const API_KEY = "adad"; // Paste your API key here
-//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(adad)
+//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
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
